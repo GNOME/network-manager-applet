b9b45320c0dd2e4bd24a63e24793a10e7d4fe52f
Lubomir Rintel <lkundrak@v3.sk>
2009-05-04 13:30:08 -0400

    fix use-after-free in vpn import error path (rh #498503)

--- src/connection-editor/vpn-helpers.c
+++ src/connection-editor/vpn-helpers.c
@@ -211,7 +211,6 @@ import_vpn_from_file_cb (GtkWidget *dialog, gint response, gpointer user_data)
 	import_info.connection = NULL;
 	import_info.error = NULL;
 	g_hash_table_foreach (plugins, try_import, (gpointer) &import_info);
-	g_free (import_info.filename);
 
 	connection = import_info.connection;
 	if (connection) {
@@ -239,6 +238,7 @@ import_vpn_from_file_cb (GtkWidget *dialog, gint response, gpointer user_data)
 
 	if (import_info.error)
 		g_error_free (import_info.error);
+	g_free (filename);
 
 out:
 	gtk_widget_hide (dialog);
