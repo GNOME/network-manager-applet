GNOME/network-manager-applet

commit 28997329dab2e260c13d53376aae6c6a477fc804

src/applet.c src/applet.h - (constructor): check whether the notification
2009-03-20  Alexander Sack <asac@ubuntu.com>

	* src/applet.c
	  src/applet.h
		- (constructor): check whether the notification server supports
			actions
		- (applet_do_notify): don't set a notification if the notification
			server doesn't support them


svn path=/trunk/; revision=1226

src/applet.c
@@ -70,6 +70,7 @@
 #include "utils.h"
 #include "gconf-helpers.h"
 
+#define NOTIFY_CAPS_ACTIONS_KEY "actions"
 
 G_DEFINE_TYPE(NMApplet, nma, G_TYPE_OBJECT)
 
@@ -396,7 +397,7 @@ applet_do_notify (NMApplet *applet,
 	notify_notification_set_urgency (notify, urgency);
 	notify_notification_set_timeout (notify, NOTIFY_EXPIRES_DEFAULT);
 
-	if (action1) {
+	if (applet->notify_with_actions && action1) {
 		notify_notification_add_action (notify, action1, action1_label,
 		                                action1_cb, action1_user_data, NULL);
 	}
@@ -2477,6 +2478,7 @@ constructor (GType type,
 {
 	NMApplet *applet;
 	AppletDBusManager *dbus_mgr;
+	GList *server_caps;
 
 	applet = NM_APPLET (G_OBJECT_CLASS (nma_parent_class)->constructor (type, n_props, construct_props));
 
@@ -2506,6 +2508,15 @@ constructor (GType type,
 
 	if (!notify_is_initted ())
 		notify_init ("NetworkManager");
+
+	server_caps = notify_get_server_caps();
+	if (g_list_find (server_caps, NOTIFY_CAPS_ACTIONS_KEY))
+		applet->notify_with_actions = TRUE;
+	else
+		applet->notify_with_actions = FALSE;
+
+	g_list_foreach (server_caps, (GFunc) g_free, NULL);
+	g_list_free (server_caps);
 
 	dbus_mgr = applet_dbus_manager_get ();
 	if (dbus_mgr == NULL) {

src/applet.h
@@ -143,6 +143,7 @@ typedef struct
 
 	GladeXML *		info_dialog_xml;
 	NotifyNotification*	notification;
+	gboolean		notify_with_actions;
 } NMApplet;
 
 

commit 29f8dd9a5050a307370894810437928fadf86448

fix notification actions broken by 28997329dab2e260c13d53376aae6c6a477fc804

src/applet.c
@@ -2478,7 +2478,7 @@ constructor (GType type,
 {
 	NMApplet *applet;
 	AppletDBusManager *dbus_mgr;
-	GList *server_caps;
+	GList *server_caps, *iter;
 
 	applet = NM_APPLET (G_OBJECT_CLASS (nma_parent_class)->constructor (type, n_props, construct_props));
 
@@ -2510,10 +2510,11 @@ constructor (GType type,
 		notify_init ("NetworkManager");
 
 	server_caps = notify_get_server_caps();
-	if (g_list_find (server_caps, NOTIFY_CAPS_ACTIONS_KEY))
-		applet->notify_with_actions = TRUE;
-	else
-		applet->notify_with_actions = FALSE;
+	applet->notify_with_actions = FALSE;
+	for (iter = server_caps; iter; iter = g_list_next (iter)) {
+		if (!strcmp ((const char *) iter->data, NOTIFY_CAPS_ACTIONS_KEY))
+			applet->notify_with_actions = TRUE;
+	}
 
 	g_list_foreach (server_caps, (GFunc) g_free, NULL);
 	g_list_free (server_caps);