5a09e69dd4024f850eea937ff7b5cd2b25518e27
Jiří Klimeš <jklimes@redhat.com>
2013-03-12 13:23:21 +0100

    editor: zero next_hop, else empty "Gateway" is filled with random bytes
    Found out while analyzing https://bugzilla.gnome.org/show_bug.cgi?id=695572

--- src/connection-editor/ip6-routes-dialog.c
+++ src/connection-editor/ip6-routes-dialog.c
@@ -17,7 +17,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (C) Copyright 2008 - 2012 Red Hat, Inc.
+ * (C) Copyright 2008 - 2013 Red Hat, Inc.
  */
 
 #include "config.h"
@@ -830,6 +830,7 @@ ip6_routes_dialog_update_setting (GtkWidget *dialog, NMSettingIP6Config *s_ip6)
 		}
 
 		/* Next hop (optional) */
+		memset (&next_hop, 0, sizeof (struct in6_addr));
 		if (!get_one_addr (model, &tree_iter, COL_NEXT_HOP, FALSE, &next_hop)) {
 			g_warning ("%s: IPv6 next hop invalid!", __func__);
 			goto next;
