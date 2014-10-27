6df226246e69a70a9e203ef2226ea018eed6898c
Lubomir Rintel <lkundrak@v3.sk>
2014-10-27 11:54:57 +0100

    editor: Add support for creating Bluetooth connections
    We need to know if the connection is going to be PAN or DUN.

--- src/connection-editor/connection-helpers.c
+++ src/connection-editor/connection-helpers.c
@@ -29,6 +29,7 @@
 #include "page-wifi.h"
 #include "page-mobile.h"
 #include "page-wimax.h"
+#include "page-bluetooth.h"
 #include "page-dsl.h"
 #include "page-infiniband.h"
 #include "page-bond.h"
@@ -128,6 +129,7 @@ get_connection_type_list (void)
 	                    NM_TYPE_SETTING_CDMA,
 	                    NM_TYPE_SETTING_BLUETOOTH,
 	                    FALSE);
+	add_type_data_real (array, _("Bluetooth"), bluetooth_connection_new, NM_TYPE_SETTING_BLUETOOTH);
 	add_type_data_real (array, _("WiMAX"), wimax_connection_new, NM_TYPE_SETTING_WIMAX);
 	add_type_data_real (array, _("DSL"), dsl_connection_new, NM_TYPE_SETTING_PPPOE);
 	add_type_data_real (array, _("InfiniBand"), infiniband_connection_new, NM_TYPE_SETTING_INFINIBAND);
