5886f7393ef3aff2f3786fa4ce7b3b34ba38d47f
Piotr Drąg <piotrdrag@gmail.com>
2015-09-12 20:41:00 +0200

    Fix a typo (addreess)

--- src/connection-editor/ce-page.c
+++ src/connection-editor/ce-page.c
@@ -307,7 +307,7 @@ ce_page_mac_entry_valid (GtkEntry *entry, int type, const char *property_name, G
 	    &&  !_mac_is_valid (mac, type)) {
 		const char *addr_type;
 
-		addr_type = type == ARPHRD_ETHER ? _("MAC address") : _("HW addreess");
+		addr_type = type == ARPHRD_ETHER ? _("MAC address") : _("HW address");
 		if (property_name) {
 			g_set_error (error, NMA_ERROR, NMA_ERROR_GENERIC,
 			             _("invalid %s for %s (%s)"),
