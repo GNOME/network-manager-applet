GNOME/network-manager-applet

commit baaae14a2d8ba98b15982240b8066e246015c8bf

c-e: add additional detail_data argument to PageNewConnectionFunc
For VPNs, we want to pass on more information.

src/connection-editor/page-vpn.c
@@ -281,6 +281,7 @@ vpn_type_result_func (NMConnection *connection, gpointer user_data)
 void
 vpn_connection_new (GtkWindow *parent,
                     const char *detail,
+                    gpointer detail_data,
                     NMClient *client,
                     PageNewConnectionResultFunc result_func,
                     gpointer user_data)

src/connection-editor/page-vpn.h
@@ -56,6 +56,7 @@ gboolean ce_page_vpn_can_export (CEPageVpn *page);
 
 void vpn_connection_new (GtkWindow *parent,
                          const char *detail,
+                         gpointer detail_data,
                          NMClient *client,
                          PageNewConnectionResultFunc result_func,
                          gpointer user_data);

src/connection-editor/ce-page.h
@@ -45,6 +45,7 @@ typedef GSList * (*PageGetConnectionsFunc) (gpointer user_data);
 
 typedef void (*PageNewConnectionFunc) (GtkWindow *parent,
                                        const char *detail,
+                                       gpointer detail_data,
                                        NMClient *client,
                                        PageNewConnectionResultFunc result_func,
                                        gpointer user_data);

src/connection-editor/connection-helpers.c
@@ -372,6 +372,7 @@ new_connection_result (NMConnection *connection,
 void
 new_connection_of_type (GtkWindow *parent_window,
                         const char *detail,
+                        gpointer detail_data,
                         NMClient *client,
                         PageNewConnectionFunc new_func,
                         NewConnectionResultFunc result_func,
@@ -387,6 +388,7 @@ new_connection_of_type (GtkWindow *parent_window,
 
 	new_func (parent_window,
 	          detail,
+	          detail_data,
 	          client,
 	          new_connection_result,
 	          ncd);
@@ -424,6 +426,7 @@ new_connection_dialog_full (GtkWindow *parent_window,
 	int response;
 	PageNewConnectionFunc new_func = NULL;
 	gs_free char *vpn_service_type = NULL;
+	const char *detail = NULL;
 	GError *error = NULL;
 
 	/* load GUI */
@@ -460,13 +463,15 @@ new_connection_dialog_full (GtkWindow *parent_window,
 		                    COL_NEW_FUNC, &new_func,
 		                    COL_VPN_SERVICE_TYPE, &vpn_service_type,
 		                    -1);
+		if (vpn_service_type)
+			detail = vpn_service_type;
 	}
 
 	gtk_widget_destroy (GTK_WIDGET (type_dialog));
 	g_object_unref (gui);
 
 	if (new_func)
-		new_connection_of_type (parent_window, vpn_service_type, client, new_func, result_func, user_data);
+		new_connection_of_type (parent_window, detail, NULL, client, new_func, result_func, user_data);
 	else
 		result_func (NULL, user_data);
 }

src/connection-editor/connection-helpers.h
@@ -54,6 +54,7 @@ void new_connection_dialog_full (GtkWindow *parent_window,
 
 void new_connection_of_type (GtkWindow *parent_window,
                              const char *detail,
+                             gpointer detail_data,
                              NMClient *client,
                              PageNewConnectionFunc new_func,
                              NewConnectionResultFunc result_func,

src/connection-editor/nm-connection-list.c
@@ -917,6 +917,7 @@ nm_connection_list_create (NMConnectionList *self, GType ctype, const char *deta
 	} else {
 		new_connection_of_type (GTK_WINDOW (self->dialog),
 		                        detail,
+		                        NULL,
 		                        self->client,
 		                        types[i].new_connection_func,
 		                        really_add_connection,

src/connection-editor/page-bluetooth.c
@@ -284,6 +284,7 @@ out:
 void
 bluetooth_connection_new (GtkWindow *parent,
                           const char *detail,
+                          gpointer detail_data,
                           NMClient *client,
                           PageNewConnectionResultFunc result_func,
                           gpointer user_data)

src/connection-editor/page-bluetooth.h
@@ -56,6 +56,7 @@ CEPage *ce_page_bluetooth_new (NMConnectionEditor *edit,
 
 void bluetooth_connection_new (GtkWindow *parent,
                                const char *detail,
+                               gpointer detail_data,
                                NMClient *client,
                                PageNewConnectionResultFunc result_func,
                                gpointer user_data);

commit 2ae7b613ac0fda298dde78fa00b781a374f60be4

c-e: fix crash during VPN import from file
Fixes: baaae14a2d8ba98b15982240b8066e246015c8bf

src/connection-editor/page-vpn.c
@@ -249,6 +249,7 @@ import_cb (NMConnection *connection, gpointer user_data)
 void
 vpn_connection_import (GtkWindow *parent,
                        const char *detail,
+                       gpointer detail_data,
                        NMClient *client,
                        PageNewConnectionResultFunc result_func,
                        gpointer user_data)

src/connection-editor/page-vpn.h
@@ -68,6 +68,7 @@ void vpn_connection_new (GtkWindow *parent,
 
 void vpn_connection_import (GtkWindow *parent,
                             const char *detail,
+                            gpointer detail_data,
                             NMClient *client,
                             PageNewConnectionResultFunc result_func,
                             gpointer user_data);