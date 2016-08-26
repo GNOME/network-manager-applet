GNOME/network-manager-applet

commit c21d56dd22057103c8125a49307b4ff47b5b644d

connection-editor: fix wired 802-1x secrets saving
In case of wired 802-1x connection a temporary connection object is used,
but since NM commit 6a19e68a7d856629600655a076b0cea4be346799
secrets are cleared when the object is destroyed.
So duplicate 802-1x settings instead of using g_object_ref().

https://mail.gnome.org/archives/networkmanager-list/2016-May/msg00097.html

src/connection-editor/page-8021x-security.c
@@ -172,7 +172,7 @@ ce_page_validate_v (CEPage *page, NMConnection *connection, GError **error)
 			ws_802_1x_fill_connection (priv->security, "wpa_eap_auth_combo", tmp_connection);
 
 			s_8021x = nm_connection_get_setting (tmp_connection, NM_TYPE_SETTING_802_1X);
-			nm_connection_add_setting (connection, NM_SETTING (g_object_ref (s_8021x)));
+			nm_connection_add_setting (connection, nm_setting_duplicate (s_8021x));
 
 			g_object_unref (tmp_connection);
 		}

commit 2c844acc8f00d9c1d3536b114208170c9cdbe7f2

connection-editor: Fix handling of wired 802-1x security setting
Function nm_setting_duplicate() can't copy object's data.
As a result a value of the "Ignore CA certificate" checkbox was
always lost. So don't copy settings, just use g_object_ref() again
and remove the 8021x setting from the temporary object to prevent
the clearing of secrets.

https://mail.gnome.org/archives/networkmanager-list/2016-August/msg00109.html

Fixes: c21d56dd22057103c8125a49307b4ff47b5b644d

src/connection-editor/page-8021x-security.c
@@ -172,8 +172,12 @@ ce_page_validate_v (CEPage *page, NMConnection *connection, GError **error)
 			ws_802_1x_fill_connection (priv->security, "wpa_eap_auth_combo", tmp_connection);
 
 			s_8021x = nm_connection_get_setting (tmp_connection, NM_TYPE_SETTING_802_1X);
-			nm_connection_add_setting (connection, nm_setting_duplicate (s_8021x));
+			nm_connection_add_setting (connection, NM_SETTING (g_object_ref (s_8021x)));
 
+			/* Remove the 8021x setting to prevent the clearing of secrets when the
+			 * simple-connection is destroyed.
+			 */
+			nm_connection_remove_setting (tmp_connection, NM_TYPE_SETTING_802_1X);
 			g_object_unref (tmp_connection);
 		}
 	} else {