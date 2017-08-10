GNOME/network-manager-applet

commit 35736dd2dfe05534e5e45d9e3cdddf3cabec840c

applet: use default active interface, even if it is of unsupported class
Currently if I have bond interface running as my default interface and
I try to establish VPN connection I get route to VPN server added to
physical ethernet interface - one of bond slaves. This makes no sense
- that slave doesn't even have IP address, and obviously such VPN
connection fails to work.

The problem happanes because we try to determine what interface has
default route and we skip all interfaces that we do not know
about (i.e. anything that is not ethernet, wifi, wwan, modem and
BT). Since bond is not on the list code skips it.

It looks like code doesn't really care what type the 'default'
interface is - so that check is really not nessesary. Removing that
check fixes 'VPN with default route via bond' problem.

Signed-off-by: Nikolay Martynov <mar.kolya@gmail.com>

https://mail.gnome.org/archives/networkmanager-list/2017-May/msg00042.html

src/applet.c
@@ -248,8 +248,12 @@ applet_get_default_active_connection (NMApplet *applet, NMDevice **device)
 			continue;
 
 		candidate_dev = g_ptr_array_index (devices, 0);
-		if (!get_device_class (candidate_dev, applet))
-			continue;
+
+		/* We have to return default connection/device even if they are of an
+		 * unknown class - otherwise we may end up returning non
+		 * default interface which has nothing to do with our default
+		 * route, e.g. we may return slave ethernet when we have
+		 * defult route going through bond */
 
 		if (nm_active_connection_get_default (candidate)) {
 			if (!default_ac) {

commit b82db07d652df24e9286d0328462f94183d406c1

applet: fix status icon when a VPN has the default route
After commit 35736dd2dfe0 ("applet: use default active interface, even
if it is of unsupported class") when a VPN with default route is
active, applet_get_default_active_connection() returns the VPN
connection, and no icon would be set because the tun device doesn't
have a NMADeviceClass.

Require a device class only when we need to determine the status icon,
otherwise accept any connection.

Fixes: 35736dd2dfe05534e5e45d9e3cdddf3cabec840c

https://bugzilla.gnome.org/show_bug.cgi?id=785775
https://bugzilla.redhat.com/show_bug.cgi?id=1471510

src/applet.c
@@ -225,7 +225,8 @@ applet_get_best_activating_connection (NMApplet *applet, NMDevice **device)
 }
 
 static NMActiveConnection *
-applet_get_default_active_connection (NMApplet *applet, NMDevice **device)
+applet_get_default_active_connection (NMApplet *applet, NMDevice **device,
+                                      gboolean only_known_devices)
 {
 	NMActiveConnection *default_ac = NULL;
 	NMDevice *non_default_device = NULL;
@@ -248,6 +249,10 @@ applet_get_default_active_connection (NMApplet *applet, NMDevice **device)
 			continue;
 
 		candidate_dev = g_ptr_array_index (devices, 0);
+
+		if (   only_known_devices
+		    && !get_device_class (candidate_dev, applet))
+			continue;
 
 		/* We have to return default connection/device even if they are of an
 		 * unknown class - otherwise we may end up returning non
@@ -1038,7 +1043,7 @@ nma_menu_vpn_item_clicked (GtkMenuItem *item, gpointer user_data)
 		return;
 	}
 
-	active = applet_get_default_active_connection (applet, &device);
+	active = applet_get_default_active_connection (applet, &device, FALSE);
 	if (!active || !device) {
 		g_warning ("%s: no active connection or device.", __func__);
 		return;
@@ -2582,7 +2587,7 @@ applet_get_device_icon_for_state (NMApplet *applet,
 		/* If there aren't any activating devices, then show the state of
 		 * the default active connection instead.
 		 */
-		active = applet_get_default_active_connection (applet, &device);
+		active = applet_get_default_active_connection (applet, &device, TRUE);
 		if (!active || !device)
 			goto out;
 	}