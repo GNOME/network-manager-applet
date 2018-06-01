GNOME/network-manager-applet

commit 15ac92e6a42317e86ffb6d52a9ce5cacbf9e8c87

all: fix bad function pointer casts
This makes GCC 8 unhappy, almost sad.

src/ap-menu-item.c
@@ -326,7 +326,7 @@ finalize (GObject *object)
 	g_free (priv->hash);
 	g_free (priv->ssid_string);
 
-	g_slist_foreach (priv->dupes, (GFunc) g_free, NULL);
+	g_slist_free_full (priv->dupes, g_free);
 	g_slist_free (priv->dupes);
 
 	G_OBJECT_CLASS (nm_network_menu_item_parent_class)->finalize (object);

src/applet-device-broadband.c
@@ -668,8 +668,10 @@ typedef struct {
 } BroadbandMenuItemInfo;
 
 static void
-menu_item_info_destroy (BroadbandMenuItemInfo *info)
+menu_item_info_destroy (gpointer data, GClosure *closure)
 {
+	BroadbandMenuItemInfo *info = data;
+
 	g_object_unref (G_OBJECT (info->device));
 	if (info->connection)
 		g_object_unref (info->connection);

src/applet-device-wifi.c
@@ -294,7 +294,7 @@ typedef struct {
 } WifiMenuItemInfo;
 
 static void
-wifi_menu_item_info_destroy (gpointer data)
+wifi_menu_item_info_destroy (gpointer data, GClosure *closure)
 {
 	WifiMenuItemInfo *info = (WifiMenuItemInfo *) data;
 
@@ -652,7 +652,7 @@ create_new_ap_item (NMDeviceWifi *device,
 			g_signal_connect_data (subitem, "activate",
 			                       G_CALLBACK (wifi_menu_item_activate),
 			                       info,
-			                       (GClosureNotify) wifi_menu_item_info_destroy, 0);
+			                       wifi_menu_item_info_destroy, 0);
 
 			gtk_menu_shell_append (GTK_MENU_SHELL (submenu), GTK_WIDGET (subitem));
 			gtk_widget_show (subitem);
@@ -676,7 +676,7 @@ create_new_ap_item (NMDeviceWifi *device,
 		                       "activate",
 		                       G_CALLBACK (wifi_menu_item_activate),
 		                       info,
-		                       (GClosureNotify) wifi_menu_item_info_destroy,
+		                       wifi_menu_item_info_destroy,
 		                       0);
 	}
 

src/applet-vpn-request.c
@@ -470,8 +470,6 @@ request_data_free (RequestData *req_data)
 		}
 	}
 
-	g_slist_foreach (req_data->lines, (GFunc) g_free, NULL);
-	g_slist_free (req_data->lines);
-
+	g_slist_free_full (req_data->lines, g_free);
 	g_slice_free (RequestData, req_data);
 }

src/applet.c
@@ -703,8 +703,7 @@ applet_notify_server_has_actions (void)
 			break;
 		}
 	}
-	g_list_foreach (server_caps, (GFunc) g_free, NULL);
-	g_list_free (server_caps);
+	g_list_free_full (server_caps, g_free);
 
 	return has_actions;
 }
@@ -1263,8 +1262,10 @@ struct AppletDeviceMenuInfo {
 };
 
 static void
-applet_device_info_destroy (struct AppletDeviceMenuInfo *info)
+applet_device_info_destroy (gpointer data, GClosure *closure)
 {
+	struct AppletDeviceMenuInfo *info = data;
+
 	g_return_if_fail (info != NULL);
 
 	if (info->device)
@@ -1324,7 +1325,7 @@ nma_menu_device_get_menu_item (NMDevice *device,
 		g_signal_connect_data (item, "activate",
 		                       G_CALLBACK (applet_device_disconnect_db),
 		                       info,
-		                       (GClosureNotify) applet_device_info_destroy, 0);
+		                       applet_device_info_destroy, 0);
 		gtk_widget_set_sensitive (item, TRUE);
 		break;
 	}

src/connection-editor/ip4-routes-dialog.c
@@ -161,8 +161,7 @@ route_delete_clicked (GtkButton *button, gpointer user_data)
 	if (gtk_tree_model_get_iter (model, &iter, (GtkTreePath *) selected_rows->data))
 		gtk_list_store_remove (GTK_LIST_STORE (model), &iter);
 
-	g_list_foreach (selected_rows, (GFunc) gtk_tree_path_free, NULL);
-	g_list_free (selected_rows);
+	g_list_free_full (selected_rows, (GDestroyNotify) gtk_tree_path_free);
 
 	num_rows = gtk_tree_model_iter_n_children (model, NULL);
 	if (num_rows && gtk_tree_model_iter_nth_child (model, &iter, NULL, num_rows - 1)) {

src/connection-editor/ip6-routes-dialog.c
@@ -196,8 +196,7 @@ route_delete_clicked (GtkButton *button, gpointer user_data)
 	if (gtk_tree_model_get_iter (model, &iter, (GtkTreePath *) selected_rows->data))
 		gtk_list_store_remove (GTK_LIST_STORE (model), &iter);
 
-	g_list_foreach (selected_rows, (GFunc) gtk_tree_path_free, NULL);
-	g_list_free (selected_rows);
+	g_list_free_full (selected_rows, (GDestroyNotify) gtk_tree_path_free);
 
 	num_rows = gtk_tree_model_iter_n_children (model, NULL);
 	if (num_rows && gtk_tree_model_iter_nth_child (model, &iter, NULL, num_rows - 1)) {

src/connection-editor/nm-connection-list.c
@@ -95,9 +95,7 @@ get_active_connection (GtkTreeView *treeview)
 	if (gtk_tree_model_get_iter (model, &iter, (GtkTreePath *) selected_rows->data))
 		gtk_tree_model_get (model, &iter, COL_CONNECTION, &connection, -1);
 
-	/* free memory */
-	g_list_foreach (selected_rows, (GFunc) gtk_tree_path_free, NULL);
-	g_list_free (selected_rows);
+	g_list_free_full (selected_rows, (GDestroyNotify) gtk_tree_path_free);
 
 	/* gtk_tree_model_get() will have reffed connection, but we don't
 	 * need that since we know the model will continue to hold a ref.

commit 40a6dd2f22c32ec668287d019ef6667c297d17ec

NMNetworkMenuItem: Don't double-free priv->dupes
https://bugs.archlinux.org/task/58767

https://gitlab.gnome.org/GNOME/network-manager-applet/merge_requests/2

Fixes: 15ac92e6a42317e86ffb6d52a9ce5cacbf9e8c87

src/ap-menu-item.c
@@ -327,7 +327,6 @@ finalize (GObject *object)
 	g_free (priv->ssid_string);
 
 	g_slist_free_full (priv->dupes, g_free);
-	g_slist_free (priv->dupes);
 
 	G_OBJECT_CLASS (nm_network_menu_item_parent_class)->finalize (object);
 }