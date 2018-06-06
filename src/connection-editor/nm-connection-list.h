37c8d7014705d1593e8e4e57ef12b9a01d0a051c
Lubomir Rintel <lkundrak@v3.sk>
2018-06-06 22:16:10 +0200

    editor/connection-list: add new-editor signal
    This indicates that a new editor window has been opened, for the
    purposes of tracking the windows as GtkApplication windows.

--- src/connection-editor/nm-connection-list.h
+++ src/connection-editor/nm-connection-list.h
@@ -30,9 +30,13 @@
 
 #include <NetworkManager.h>
 
+#include "nm-connection-editor.h"
+
 #define NM_TYPE_CONNECTION_LIST    (nm_connection_list_get_type ())
 #define NM_IS_CONNECTION_LIST(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NM_TYPE_CONNECTION_LIST))
 #define NM_CONNECTION_LIST(obj)    (G_TYPE_CHECK_INSTANCE_CAST ((obj), NM_TYPE_CONNECTION_LIST, NMConnectionList))
+
+#define NM_CONNECTION_LIST_NEW_EDITOR "new-editor"
 
 typedef struct _NMConnectionListPrivate NMConnectionListPrivate;
 
