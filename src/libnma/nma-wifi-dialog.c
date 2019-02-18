GNOME/network-manager-applet

commit 755cc5647f4ee1bb90db6fa884ff1e7105c2d321

libnma: free buffers returned by validate_dialog_ssid

src/libnma/nma-wifi-dialog.c
@@ -309,6 +309,8 @@ ssid_entry_changed (GtkWidget *entry, gpointer user_data)
 	if (!ssid)
 		goto out;
 
+	g_bytes_unref (ssid);
+
 	model = gtk_combo_box_get_model (GTK_COMBO_BOX (priv->sec_combo));
 	if (gtk_combo_box_get_active_iter (GTK_COMBO_BOX (priv->sec_combo), &iter))
 		gtk_tree_model_get (model, &iter, S_SEC_COLUMN, &sec, -1);
@@ -1190,6 +1192,7 @@ nma_wifi_dialog_get_connection (NMAWifiDialog *self,
 	if (!priv->connection) {
 		NMSettingConnection *s_con;
 		char *uuid;
+		GBytes *ssid;
 
 		connection = nm_simple_connection_new ();
 
@@ -1203,7 +1206,9 @@ nma_wifi_dialog_get_connection (NMAWifiDialog *self,
 		nm_connection_add_setting (connection, (NMSetting *) s_con);
 
 		s_wireless = (NMSettingWireless *) nm_setting_wireless_new ();
-		g_object_set (s_wireless, NM_SETTING_WIRELESS_SSID, validate_dialog_ssid (self), NULL);
+		ssid = validate_dialog_ssid (self);
+		g_object_set (s_wireless, NM_SETTING_WIRELESS_SSID, ssid, NULL);
+		g_free (ssid);
 
 		if (priv->operation == OP_CREATE_ADHOC) {
 			NMSetting *s_ip4;

commit b4c365b136e25e69c3550a9984f8f3cbec4bc073

wifi-dialog: fix wrong free
Fixes: 755cc5647f4ee1bb90db6fa884ff1e7105c2d321

https://gitlab.gnome.org/GNOME/network-manager-applet/issues/43

src/libnma/nma-wifi-dialog.c
@@ -1260,7 +1260,7 @@ nma_wifi_dialog_get_connection (NMAWifiDialog *self,
 		s_wireless = (NMSettingWireless *) nm_setting_wireless_new ();
 		ssid = validate_dialog_ssid (self);
 		g_object_set (s_wireless, NM_SETTING_WIRELESS_SSID, ssid, NULL);
-		g_free (ssid);
+		g_bytes_unref (ssid);
 
 		if (priv->operation == OP_CREATE_ADHOC) {
 			NMSetting *s_ip4;