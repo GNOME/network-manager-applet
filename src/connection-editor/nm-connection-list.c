GNOME/network-manager-applet

commit 4645c9ae9dd6edd41f9cae48f2de26b39f884795

editor: filter the connection type headers too

src/connection-editor/nm-connection-list.c
@@ -642,6 +642,33 @@ timestamp_sort_func (GtkTreeModel *model, GtkTreeIter *a, GtkTreeIter *b, gpoint
 }
 
 static gboolean
+has_visible_children (NMConnectionList *self, GtkTreeModel *model, GtkTreeIter *parent)
+{
+	NMConnectionListPrivate *priv = NM_CONNECTION_LIST_GET_PRIVATE (self);
+	GtkTreeIter iter;
+	const char *search;
+	char *id = NULL;
+
+	if (!gtk_search_bar_get_search_mode (priv->search_bar))
+		return gtk_tree_model_iter_has_child  (model, parent);
+
+	if (!gtk_tree_model_iter_children (model, &iter, parent))
+		return FALSE;
+
+	search = gtk_entry_get_text (GTK_ENTRY (priv->search_entry));
+	do {
+		gtk_tree_model_get (model, &iter, COL_ID, &id, -1);
+		if (strcasestr (id, search) != NULL) {
+			g_free (id);
+			return TRUE;
+		}
+		g_free (id);
+	} while (gtk_tree_model_iter_next (model, &iter));
+
+	return FALSE;
+}
+
+static gboolean
 tree_model_visible_func (GtkTreeModel *model,
                          GtkTreeIter *iter,
                          gpointer user_data)
@@ -659,8 +686,8 @@ tree_model_visible_func (GtkTreeModel *model,
 	                    COL_CONNECTION, &connection,
 	                    -1);
 	if (!connection) {
-		/* Top-level type nodes are visible iff they have children */
-		return gtk_tree_model_iter_has_child  (model, iter);
+		/* Top-level type nodes are visible iff they have visible children */
+		return has_visible_children (self, model, iter);
 	}
 
 	if (   gtk_search_bar_get_search_mode (priv->search_bar)

commit 9ef81a0820c7951e6dffc047e83d83a467c128b2

editor: avoid crash when checking for visible children
When a connection is added the editor adds a new empty item to the
tree store with gtk_tree_store_append() and then populates
it. Therefore, has_visible_children() must be prepared to deal with
missing values.

Fixes: 4645c9ae9dd6edd41f9cae48f2de26b39f884795

https://bugzilla.redhat.com/show_bug.cgi?id=1543958

src/connection-editor/nm-connection-list.c
@@ -667,6 +667,11 @@ has_visible_children (NMConnectionList *self, GtkTreeModel *model, GtkTreeIter *
 	search = gtk_entry_get_text (GTK_ENTRY (priv->search_entry));
 	do {
 		gtk_tree_model_get (model, &iter, COL_ID, &id, -1);
+		if (!id) {
+			/* gtk_tree_store_append() inserts an empty row, ignore
+			 * it until it is fully populated. */
+			continue;
+		}
 		if (strcasestr (id, search) != NULL) {
 			g_free (id);
 			return TRUE;