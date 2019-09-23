GNOME/network-manager-applet

commit 2ded57d949db06816d9f8a59cf34bc91ff69c3d6

utils: support GTK4
Cover a trivial difference with an ifdef.

src/libnma/nma-ui-utils.c
@@ -219,14 +219,32 @@ nma_gtk_widget_activate_default (GtkWidget *widget)
 static void
 icon_release_cb (GtkEntry *entry,
                  GtkEntryIconPosition position,
+#if !GTK_CHECK_VERSION(3,90,0)
                  GdkEventButton *event,
+#endif
                  gpointer data)
 {
 	GtkMenu *menu = GTK_MENU (data);
+#if GTK_CHECK_VERSION(3,90,0)
+	GdkRectangle icon_area;
+#endif
+
 	if (position == GTK_ENTRY_ICON_SECONDARY) {
-		gtk_widget_show_all (GTK_WIDGET (data));
+		gtk_widget_show (GTK_WIDGET (data));
+#if GTK_CHECK_VERSION(3,90,0)
+		gtk_entry_get_icon_area (entry,
+		                         GTK_ENTRY_ICON_SECONDARY,
+		                         &icon_area);
+		gtk_menu_popup_at_rect (menu,
+		                        gtk_widget_get_surface (GTK_WIDGET (entry)),
+		                        &icon_area,
+		                        GDK_GRAVITY_CENTER,
+		                        GDK_GRAVITY_CENTER,
+		                        NULL);
+#else
 		gtk_menu_popup (menu, NULL, NULL, NULL, NULL,
 		                event->button, event->time);
+#endif
 	}
 }
 

src/utils/utils.c
@@ -318,10 +318,19 @@ utils_override_bg_color (GtkWidget *widget, GdkRGBA *rgba)
 	if (rgba) {
 		css = g_strdup_printf ("* { background-color: %s; background-image: none; }",
 		                       gdk_rgba_to_string (rgba));
+#if GTK_CHECK_VERSION(3,90,0)
+		gtk_css_provider_load_from_data (provider, css, -1);
+#else
 		gtk_css_provider_load_from_data (provider, css, -1, NULL);
+#endif
 		g_free (css);
-	} else
+	} else {
+#if GTK_CHECK_VERSION(3,90,0)
+		gtk_css_provider_load_from_data (provider, "", -1);
+#else
 		gtk_css_provider_load_from_data (provider, "", -1, NULL);
+#endif
+	}
 }
 
 void

commit f1f1c280de83a28bbe5a57f2e13d36ef758e9086

libnma: fix password flags menu
Without gtk_widget_show_all() the popup menu for password flags is
displayed empty when using GTK3.

Fixes: 2ded57d949db ("utils: support GTK4")

src/libnma/nma-ui-utils.c
@@ -230,8 +230,8 @@ icon_release_cb (GtkEntry *entry,
 #endif
 
 	if (position == GTK_ENTRY_ICON_SECONDARY) {
-		gtk_widget_show (GTK_WIDGET (data));
 #if GTK_CHECK_VERSION(3,90,0)
+		gtk_widget_show (GTK_WIDGET (data));
 		gtk_entry_get_icon_area (entry,
 		                         GTK_ENTRY_ICON_SECONDARY,
 		                         &icon_area);
@@ -242,6 +242,7 @@ icon_release_cb (GtkEntry *entry,
 		                        GDK_GRAVITY_CENTER,
 		                        NULL);
 #else
+		gtk_widget_show_all (GTK_WIDGET (data));
 		gtk_menu_popup (menu, NULL, NULL, NULL, NULL,
 		                event->button, event->time);
 #endif