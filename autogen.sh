8f2681b36719f43bc94037cb6a981633428dc648
Lubomir Rintel <lkundrak@v3.sk>
2019-10-11 18:05:10 +0200

    all: everything but libnma alone

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME=nm-applet
 
 (test -f $srcdir/configure.ac \
-  && test -f $srcdir/src/applet.c) || {
+  && test -f $srcdir/src/nma-version.h.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
