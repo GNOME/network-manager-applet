GNOME/network-manager-applet

commit dc9ee4e1129c87aa8a79d1783375877a5f522aee

applet: scale icons for HiDPI displays
https://gitlab.gnome.org/GNOME/network-manager-applet/merge_requests/71

src/applet.c
@@ -2992,6 +2992,7 @@ nma_icon_check_and_load (const char *name, NMApplet *applet)
 {
 	GError *error = NULL;
 	GdkPixbuf *icon;
+	int scale;
 
 	g_assert (name != NULL);
 	g_assert (applet != NULL);
@@ -3000,10 +3001,12 @@ nma_icon_check_and_load (const char *name, NMApplet *applet)
 	if (g_hash_table_lookup_extended (applet->icon_cache, name, NULL, (gpointer) &icon))
 		return icon;
 
+	scale = gtk_widget_get_scale_factor (applet->context_menu);
+
 	/* Try to load the icon; if the load fails, log the problem, and set
 	 * the icon to the fallback icon if requested.
 	 */
-	if (!(icon = gtk_icon_theme_load_icon (applet->icon_theme, name, applet->icon_size, GTK_ICON_LOOKUP_FORCE_SIZE, &error))) {
+	if (!(icon = gtk_icon_theme_load_icon_for_scale (applet->icon_theme, name, applet->icon_size, scale, GTK_ICON_LOOKUP_FORCE_SIZE, &error))) {
 		g_warning ("failed to load icon \"%s\": %s", name, error->message);
 		g_clear_error (&error);
 		icon = nm_g_object_ref (applet->fallback_icon);

commit 360ad330ed2dc4c34a40e418a2a8c642459dd4f9

applet: fix widget assertion failure when running as indicator
Fixes: dc9ee4e1129c ('applet: scale icons for HiDPI displays')

https://gitlab.gnome.org/GNOME/network-manager-applet/merge_requests/74

src/applet.c
@@ -3001,7 +3001,7 @@ nma_icon_check_and_load (const char *name, NMApplet *applet)
 	if (g_hash_table_lookup_extended (applet->icon_cache, name, NULL, (gpointer) &icon))
 		return icon;
 
-	scale = gtk_widget_get_scale_factor (applet->context_menu);
+	scale = gdk_window_get_scale_factor (gdk_get_default_root_window ());
 
 	/* Try to load the icon; if the load fails, log the problem, and set
 	 * the icon to the fallback icon if requested.