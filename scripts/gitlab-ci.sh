b90d24b687d7276f6c61591bf47c67c0e5f90f7a
Thomas Haller <thaller@redhat.com>
2020-03-07 10:03:53 +0100

    gitlab-ci: move logic from gitlab-ci.yml to script

---
+++ scripts/gitlab-ci.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+die() {
+    echo ">>> FAIL: $@"
+    exit 1
+}
+
+fedora_pkg_minimal() {
+    echo \
+        /usr/bin/autopoint \
+        NetworkManager-libnm-devel \
+        desktop-file-utils \
+        fedora-repos-rawhide \
+        file \
+        findutils \
+        gcc \
+        gettext-devel \
+        glib2-devel \
+        gtk3-devel \
+        libgudev1-devel \
+        libnotify-devel \
+        libsecret-devel \
+        libtool \
+        pkgconfig \
+        ;
+}
+
+fedora_pkg_full() {
+    echo \
+        $(fedora_pkg_minimal) \
+        redhat-rpm-config \
+        jansson-devel \
+        ModemManager-glib-devel \
+        libselinux-devel \
+        ;
+}
+
+fedora_pkg_build() {
+    case "$1" in
+        autotools)
+            echo autoconf automake make
+            ;;
+        make)
+            echo make
+            ;;
+        meson)
+            echo meson
+            ;;
+        "")
+            ;;
+        *)
+            die "Unknown build option \"$1\""
+            ;;
+    esac
+}
+
+fedora_install_libnma() {
+    pushd /etc/yum.repos.d
+        $CMD curl 'https://gitlab.gnome.org/thaller/network-manager-applet/-/raw/96bd01b5ef1a7e24bcf4bc945aa7465ac02915a8/libnma-local.tgz' --output ./libnma-local.tgz
+        $CMD tar -xvzf ./libnma-local.tgz
+        $CMD dnf -y install --nogpgcheck --enablerepo=libnma-local libnma-devel
+    popd
+}
+
+fedora_install_minimal() {
+    $CMD dnf -y install $(fedora_pkg_minimal) $(fedora_pkg_build "$1")
+    fedora_install_libnma
+}
+
+fedora_install_full() {
+    $CMD dnf -y install $(fedora_pkg_full) $(fedora_pkg_build "$1")
+    fedora_install_libnma
+}
+
+set -xe
+
+"$1" "$2"
