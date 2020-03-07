240da3ea68caa9eac516d5c2bd993c0eb7cc58be
Thomas Haller <thaller@redhat.com>
2020-03-07 10:03:53 +0100

    gitlab-ci: move logic from gitlab-ci.yml to script and install external libnma-1.8.28
    I think a plain shell script makes it easier to implement the build
    steps. It's easier to read and to extend.
    
    Also, the gitlab-ci script tried to install libnma from rawhide.
    However, while the libnma-1.8.26 package is currently built in koji,
    it is still (for some reasons) not available for installation. The
    build step would still install libnma-1.8.24 (which was still bundled
    with applet). Aside that, there are currently other issues in rawhide
    that prevent this from working.
    
    This only worked accidentally, because the network-manager-applet build
    does not yet explicitly require a new libnma package. However, in practice
    it does already, because when building against libnma-2.8.24, the
    "org.gnome.nm-applet.gschema.xml" is missing. Next we are going to explicitly
    require libnma-1.8.28, so this wouldn't work anymore.
    
    Instead I did a scratch build of libnma-1.8.28 in koji. Adjust the
    build steps and install the package. This is a temporary solution,
    that helps to boot strap the update to an unbundled libnma-1.8.28.

---
+++ scripts/gitlab-ci.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+die() {
+    echo ">>> FAIL: $@"
+    exit 1
+}
+
+is_in_set() {
+    local v="$1"
+    shift
+    for w ; do
+        [[ "$v" == "$w" ]] && return 0
+    done
+    return 1
+}
+
+fedora_pkg_minimal() {
+    echo \
+        /usr/bin/autopoint \
+        NetworkManager-libnm-devel \
+        desktop-file-utils \
+        fedora-repos-rawhide \
+        file \
+        findutils \
+        gcc \
+        gettext-devel \
+        glib2-devel \
+        gtk3-devel \
+        libgudev1-devel \
+        libnotify-devel \
+        libsecret-devel \
+        libtool \
+        pkgconfig \
+        #
+}
+
+fedora_pkg_full() {
+    echo \
+        $(fedora_pkg_minimal) \
+        redhat-rpm-config \
+        jansson-devel \
+        ModemManager-glib-devel \
+        libselinux-devel \
+        #
+}
+
+fedora_pkg_build() {
+    case "$1" in
+        autotools)
+            echo autoconf automake make
+            ;;
+        make)
+            echo make
+            ;;
+        meson)
+            echo meson
+            ;;
+        "")
+            ;;
+        *)
+            die "Unknown build option \"$1\""
+            ;;
+    esac
+}
+
+fedora_install_libnma() {
+    pushd /etc/yum.repos.d
+        local URL='https://gitlab.gnome.org/thaller/network-manager-applet/-/raw/e9d885749b281e86fb92421e73e1564b233141b4/nm-applet-prebuilt-rpms.tgz'
+        $CMD curl "$URL" --output ./nm-applet-prebuilt-rpms.tgz
+        $CMD tar -xvzf ./nm-applet-prebuilt-rpms.tgz
+        $CMD dnf -y install --enablerepo=nm-applet-prebuilt-rpms libnma-devel
+    popd
+}
+
+fedora_install_minimal() {
+    $CMD dnf -y install $(fedora_pkg_minimal) $(fedora_pkg_build "$1")
+    fedora_install_libnma
+}
+
+fedora_install_full() {
+    $CMD dnf -y install $(fedora_pkg_full) $(fedora_pkg_build "$1")
+    fedora_install_libnma
+}
+
+
+set -xe
+
+if is_in_set "$1" fedora_install_minimal fedora_install_full ; then
+    is_in_set "$2" autotools make meson || die "invalid argument \$2 (\"$2\")"
+    "$1" "$2"
+    exit 0
+fi
+
+die "Unknown command \"$1\""
