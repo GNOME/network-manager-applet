GNOME/network-manager-applet

commit 9b5c5eed6aa68eae373d948375adbbc3fabcea27

Revert "8021x-security: don't show_all() the widgets"
This reverts commit 546539e3c9fe40ab71ed3d62312e9768df661371.

src/connection-editor/page-8021x-security.c
@@ -88,6 +88,7 @@ finish_setup (CEPage8021xSecurity *self, gpointer user_data)
 
 	gtk_box_pack_start (GTK_BOX (parent->page), GTK_WIDGET (priv->enabled), FALSE, TRUE, 12);
 	gtk_box_pack_start (GTK_BOX (parent->page), priv->security_widget, TRUE, TRUE, 0);
+	gtk_widget_show_all (parent->page);
 }
 
 CEPage *

commit bf6b33bbe20f3680ee9cac2d936dd2bbdc139bff

editor: show() the 8021x-security page, not its children
During the construction of the security widget, some child widgets are
set as hidden because they should never appear (for example, the
private key passphrase entry for EAP-MD5). gtk_widget_show_all()
wrongly displays them again. Use gtk_widget_show() instead.

Fixes: 9b5c5eed6aa6 ("Revert '8021x-security: don't show_all() the widgets'")

src/connection-editor/page-8021x-security.c
@@ -74,7 +74,7 @@ finish_setup (CEPage8021xSecurity *self, gpointer user_data)
 
 	gtk_box_pack_start (GTK_BOX (parent->page), GTK_WIDGET (priv->enabled), FALSE, TRUE, 12);
 	gtk_box_pack_start (GTK_BOX (parent->page), priv->security_widget, TRUE, TRUE, 0);
-	gtk_widget_show_all (parent->page);
+	gtk_widget_show (parent->page);
 }
 
 CEPage *