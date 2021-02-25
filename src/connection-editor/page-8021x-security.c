GNOME/network-manager-applet

commit bf6b33bbe20f3680ee9cac2d936dd2bbdc139bff

editor: show() the 8021x-security page, not its children
During the construction of the security widget, some child widgets are
set as hidden because they should never appear (for example, the
private key passphrase entry for EAP-MD5). gtk_widget_show_all()
wrongly displays them again. Use gtk_widget_show() instead.

Fixes: 9b5c5eed6aa6 ("Revert '8021x-security: don't show_all() the widgets'")

src/connection-editor/page-8021x-security.c
@@ -74,7 +74,7 @@ finish_setup (CEPage8021xSecurity *self, gpointer user_data)
 
 	gtk_box_pack_start (GTK_BOX (parent->page), GTK_WIDGET (priv->enabled), FALSE, TRUE, 12);
 	gtk_box_pack_start (GTK_BOX (parent->page), priv->security_widget, TRUE, TRUE, 0);
-	gtk_widget_show_all (parent->page);
+	gtk_widget_show (parent->page);
 }
 
 CEPage *

commit 49f87caa3ab867de2569185c34c384d507ed5b62

editor: show missing toggle in the 8021x-security page
Fixes: bf6b33bbe20f ("editor: show() the 8021x-security page, not its children")

src/connection-editor/page-8021x-security.c
@@ -65,6 +65,7 @@ finish_setup (CEPage8021xSecurity *self, gpointer user_data)
 	if (parent_container)
 		gtk_container_remove (GTK_CONTAINER (parent_container), GTK_WIDGET (priv->security));
 
+	gtk_widget_set_visible (GTK_WIDGET (priv->enabled), TRUE);
 	gtk_toggle_button_set_active (priv->enabled, priv->initial_have_8021x);
 	g_signal_connect (priv->enabled, "toggled", G_CALLBACK (enable_toggled), self);
 	gtk_widget_set_sensitive (GTK_WIDGET (priv->security), priv->initial_have_8021x);