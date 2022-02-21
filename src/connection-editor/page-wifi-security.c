GNOME/network-manager-applet

commit b85ed300781ce9f26df9cf8e659d1bc0bc173201

connection-editor: add OWE support
This adds support for Enhanced Open support as a selectable wireless
security method.

src/connection-editor/page-wifi-security.c
@@ -132,6 +132,11 @@ get_default_type_for_security (NMSettingWirelessSecurity *sec)
 	if (!strcmp (key_mgmt, "sae"))
 		return NMU_SEC_SAE;
 
+#if NM_CHECK_VERSION(1,24,0)
+	if (!strcmp (key_mgmt, "owe"))
+		return NMU_SEC_OWE;
+#endif
+
 	return NMU_SEC_INVALID;
 }
 
@@ -424,6 +429,20 @@ finish_setup (CEPageWifiSecurity *self, gpointer user_data)
 			item++;
 		}
 	}
+
+#if NM_CHECK_VERSION(1,24,0)
+	if (security_valid (NMU_SEC_OWE, mode)) {
+		gtk_list_store_append (sec_model, &iter);
+		gtk_list_store_set (sec_model, &iter,
+		                    S_NAME_COLUMN, _("Enhanced Open"),
+		                    S_ADHOC_VALID_COLUMN, FALSE,
+		                    S_HOTSPOT_VALID_COLUMN, TRUE,
+		                    -1);
+		if ((active < 0) && (default_type == NMU_SEC_OWE))
+			active = item;
+		item++;
+	}
+#endif
 
 	combo = GTK_COMBO_BOX (gtk_builder_get_object (parent->builder, "wifi_security_combo"));
 	gtk_combo_box_set_model (combo, GTK_TREE_MODEL (sec_model));

commit 38731695f7b38d867e51b3d5cee2d0281c315a71

Wi-Fi/OWE: Fix Enhanced Open (OWE) not being saved properly from nm-connection-editor
When we create a Wi-Fi connection using Enhanced Open (OWE) Security
from nm-connection-editor and save it, it was not being saved and the
security was being set as "None", with Wireless Security Setting
being discarded. This was fixed.

This fix is temporary fix. It should (and will) be done from libnma,
OWE should be implemented in libnma. Meanwhile this gets done,
this solution handles this issue.

https://bugzilla.redhat.com/show_bug.cgi?id=2023156

Fixes: b85ed300781ce9f26df9cf8e659d1bc0bc173201

https://gitlab.gnome.org/GNOME/network-manager-applet/-/merge_requests/108

src/connection-editor/page-wifi-security.c
@@ -580,10 +580,24 @@ ce_page_validate_v (CEPage *page, NMConnection *connection, GError **error)
 
 		g_object_unref (ws);
 	} else {
-		/* No security, unencrypted */
-		nm_connection_remove_setting (connection, NM_TYPE_SETTING_WIRELESS_SECURITY);
-		nm_connection_remove_setting (connection, NM_TYPE_SETTING_802_1X);
-		valid = TRUE;
+
+		if (gtk_combo_box_get_active (priv->security_combo) == 0) {
+			/* No security, unencrypted */
+			nm_connection_remove_setting (connection, NM_TYPE_SETTING_WIRELESS_SECURITY);
+			nm_connection_remove_setting (connection, NM_TYPE_SETTING_802_1X);
+			valid = TRUE;
+		} else {
+			/* owe case:
+			 * fill the connection manually until libnma implements OWE wireless security
+			 */
+			NMSetting *s_wireless_sec;
+
+			s_wireless_sec = nm_setting_wireless_security_new ();
+			g_object_set (s_wireless_sec, NM_SETTING_WIRELESS_SECURITY_KEY_MGMT, "owe", NULL);
+			nm_connection_add_setting (connection, s_wireless_sec);
+			nm_connection_remove_setting (connection, NM_TYPE_SETTING_802_1X);
+			valid = TRUE;
+		}
 	}
 
 	return valid;