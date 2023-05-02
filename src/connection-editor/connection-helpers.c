GNOME/network-manager-applet

commit 3ff5b6cc984162c27949139306bd5acb9a6ea0e0

c-e: support importing WireGuard profiles from wg-quick files
Use new 1.40 API from libnm to import WireGuard profiles.

The following now works as expected:

  nm-connection-editor -i $WG_CONF_FILE
  nm-connection-editor -i $WG_CONF_FILE -t wireguard

This was already working before:

  nm-connection-editor -c -t wireguard

What also works is to click "Import a saved VPN connection..."
and select a wg-quick configuration file.

src/connection-editor/connection-helpers.c
@@ -163,7 +163,7 @@ no_description:
 }
 
 NMConnection *
-connection_import_from_file (const char *filename, GError **error)
+connection_import_from_file (const char *filename, GType ctype, GError **error)
 {
 	gs_free_error GError *unused_error = NULL;
 	NMConnection *connection = NULL;
@@ -180,29 +180,40 @@ connection_import_from_file (const char *filename, GError **error)
 		error = &unused_error;
 	}
 
-	for (iter = vpn_get_plugin_infos (); !connection && iter; iter = iter->next) {
-		NMVpnEditorPlugin *plugin;
+	if (NM_IN_SET (ctype, G_TYPE_INVALID, NM_TYPE_SETTING_VPN)) {
+		for (iter = vpn_get_plugin_infos (); !connection && iter; iter = iter->next) {
+			NMVpnEditorPlugin *plugin;
 
-		plugin = nm_vpn_plugin_info_get_editor_plugin (iter->data);
-		g_clear_error (error);
-		connection = nm_vpn_editor_plugin_import (plugin, filename, error);
-		if (connection)
-			break;
+			plugin = nm_vpn_plugin_info_get_editor_plugin (iter->data);
+			g_clear_error (error);
+			connection = nm_vpn_editor_plugin_import (plugin, filename, error);
+			if (connection)
+				break;
+		}
+
+		if (connection) {
+			NMSettingVpn *s_vpn;
+			const char *service_type;
+
+			s_vpn = nm_connection_get_setting_vpn (connection);
+			service_type = s_vpn ? nm_setting_vpn_get_service_type (s_vpn) : NULL;
+
+			/* Check connection sanity. */
+			if (!service_type || !strlen (service_type)) {
+				g_object_unref (connection);
+				connection = NULL;
+				g_set_error_literal (error, NMA_ERROR, NMA_ERROR_GENERIC,
+				                     _("No VPN service type."));
+			}
+		}
 	}
 
-	if (connection) {
-		NMSettingVpn *s_vpn;
-		const char *service_type;
-
-		s_vpn = nm_connection_get_setting_vpn (connection);
-		service_type = s_vpn ? nm_setting_vpn_get_service_type (s_vpn) : NULL;
-
-		/* Check connection sanity. */
-		if (!service_type || !strlen (service_type)) {
-			g_object_unref (connection);
-			connection = NULL;
-			g_set_error_literal (error, NMA_ERROR, NMA_ERROR_GENERIC, _("No VPN service type."));
-		}
+	if (!connection && NM_IN_SET (ctype, G_TYPE_INVALID, NM_TYPE_SETTING_WIREGUARD)) {
+#if NM_CHECK_VERSION(1, 40, 0)
+		G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+		connection = nm_conn_wireguard_import (filename, error && !*error ? error : NULL);
+		G_GNUC_END_IGNORE_DEPRECATIONS
+#endif
 	}
 
 	if (!connection)
@@ -237,8 +248,14 @@ import_vpn_from_file_cb (GtkWidget *dialog, gint response, gpointer user_data)
 	}
 
 	canceled = FALSE;
-	connection = connection_import_from_file (filename, &error);
-	if (connection) {
+	connection = connection_import_from_file (filename, G_TYPE_INVALID, &error);
+	if (!connection) {
+		/* pass */
+	} else if (nm_streq (nm_connection_get_connection_type (connection),
+	                     NM_SETTING_WIREGUARD_SETTING_NAME)) {
+		info->result_func (FUNC_TAG_PAGE_NEW_CONNECTION_RESULT_CALL, connection, FALSE, NULL,
+		                   info->user_data);
+	} else {
 		/* Wrap around the actual new function so that the page can complete
 		 * the missing parts, such as UUID or make up the connection name. */
 		vpn_connection_new (FUNC_TAG_PAGE_NEW_CONNECTION_CALL,

src/connection-editor/connection-helpers.h
@@ -73,7 +73,7 @@ gboolean connection_supports_proxy (NMConnection *connection);
 gboolean connection_supports_ip4 (NMConnection *connection);
 gboolean connection_supports_ip6 (NMConnection *connection);
 
-NMConnection *connection_import_from_file (const char *filename, GError **error);
+NMConnection *connection_import_from_file (const char *filename, GType ctype, GError **error);
 
 #endif  /* __CONNECTION_HELPERS_H__ */
 

src/connection-editor/nm-connection-list.c
@@ -1045,14 +1045,13 @@ nm_connection_list_create (NMConnectionList *list,
 
 	if (import_filename) {
 		if (ctype == G_TYPE_INVALID) {
-			/* Atempt a VPN import */
-			connection = connection_import_from_file (import_filename, NULL);
-			if (connection)
-				ctype = NM_TYPE_SETTING_VPN;
-			else
-				g_set_error (&error, NMA_ERROR, NMA_ERROR_GENERIC, _("Unrecognized connection type"));
-		} else if (ctype == NM_TYPE_SETTING_VPN) {
-			connection = connection_import_from_file (import_filename, &error);
+			connection = connection_import_from_file (import_filename, ctype, NULL);
+			if (!connection) {
+				g_set_error (&error, NMA_ERROR, NMA_ERROR_GENERIC,
+				             _ ("Unrecognized connection type"));
+			}
+		} else if (NM_IN_SET (ctype, NM_TYPE_SETTING_VPN, NM_TYPE_SETTING_WIREGUARD)) {
+			connection = connection_import_from_file (import_filename, ctype, &error);
 		} else {
 			g_set_error (&error, NMA_ERROR, NMA_ERROR_GENERIC,
 			             _("Don’t know how to import “%s” connections"), g_type_name (ctype));
@@ -1062,6 +1061,15 @@ nm_connection_list_create (NMConnectionList *list,
 			nm_connection_editor_error (NULL, _("Error importing connection"), "%s", error->message);
 			callback (list, user_data);
 			return;
+		}
+
+		if (nm_streq0 (nm_connection_get_connection_type (connection),
+		               NM_SETTING_WIREGUARD_SETTING_NAME))
+			ctype = NM_TYPE_SETTING_WIREGUARD;
+		else {
+			nm_assert (NM_IN_STRSET (nm_connection_get_connection_type (connection), NULL,
+			                         NM_SETTING_VPN_SETTING_NAME));
+			ctype = NM_TYPE_SETTING_VPN;
 		}
 	}
 

src/connection-editor/page-wireguard.c
@@ -629,15 +629,22 @@ wireguard_connection_new (FUNC_TAG_PAGE_NEW_CONNECTION_IMPL,
 	                             NM_SETTING_WIREGUARD_SETTING_NAME,
 	                             FALSE,
 	                             client);
-	nm_connection_add_setting (connection, nm_setting_wireguard_new ());
 
-	s_ip = (NMSettingIPConfig *) nm_setting_ip4_config_new ();
-	g_object_set (s_ip, NM_SETTING_IP_CONFIG_METHOD, NM_SETTING_IP4_CONFIG_METHOD_DISABLED, NULL);
-	nm_connection_add_setting (connection, (NMSetting *) s_ip);
+	if (!nm_connection_get_setting (connection, NM_TYPE_SETTING_WIREGUARD))
+		nm_connection_add_setting (connection, nm_setting_wireguard_new ());
 
-	s_ip = (NMSettingIPConfig *) nm_setting_ip6_config_new ();
-	g_object_set (s_ip, NM_SETTING_IP_CONFIG_METHOD, NM_SETTING_IP6_CONFIG_METHOD_IGNORE, NULL);
-	nm_connection_add_setting (connection, (NMSetting *) s_ip);
+	if (!nm_connection_get_setting (connection, NM_TYPE_SETTING_IP4_CONFIG)) {
+		s_ip = (NMSettingIPConfig *)nm_setting_ip4_config_new ();
+		g_object_set (s_ip, NM_SETTING_IP_CONFIG_METHOD, NM_SETTING_IP4_CONFIG_METHOD_DISABLED,
+		              NULL);
+		nm_connection_add_setting (connection, (NMSetting *)s_ip);
+	}
+
+	if (!nm_connection_get_setting (connection, NM_TYPE_SETTING_IP6_CONFIG)) {
+		s_ip = (NMSettingIPConfig *)nm_setting_ip6_config_new ();
+		g_object_set (s_ip, NM_SETTING_IP_CONFIG_METHOD, NM_SETTING_IP6_CONFIG_METHOD_IGNORE, NULL);
+		nm_connection_add_setting (connection, (NMSetting *)s_ip);
+	}
 
 	(*result_func) (FUNC_TAG_PAGE_NEW_CONNECTION_RESULT_CALL, connection, FALSE, NULL, user_data);
 }

commit 01281fae6b601598cd2006bc8f2d5be98810228d

c-e: fix crash in import_vpn_from_file_cb() when importing VPN profiles
Import code can create incomplete profiles, that don't have
"connection.type" set. Avoid the crash.

  #0  __strcmp_evex () at ../sysdeps/x86_64/multiarch/strcmp-evex.S:314
  #1  0x000000000043d177 in import_vpn_from_file_cb (dialog=0x7f4650, response=<optimized out>, user_data=0x7d66e0) at src/connection-editor/connection-helpers.c:275
  #2  0x00007ffff6f564ea in g_closure_invoke (closure=0x8d5120, return_value=0x0, n_param_values=2, param_values=0x7fffffffd0f0, invocation_hint=0x7fffffffd070) at ../gobject/gclosure.c:832
  #3  0x00007ffff6f84d36 in signal_emit_unlocked_R.isra.0
      (node=node@entry=0x847c70, detail=detail@entry=0, instance=instance@entry=0x7f4650, emission_return=emission_return@entry=0x0, instance_and_params=instance_and_params@entry=0x7fffffffd0f0) at ../gobject/gsignal.c:3812
  #4  0x00007ffff6f75bdd in g_signal_emit_valist (instance=<optimized out>, signal_id=<optimized out>, detail=<optimized out>, var_args=var_args@entry=0x7fffffffd2b0) at ../gobject/gsignal.c:3565
  #5  0x00007ffff6f75e53 in g_signal_emit (instance=<optimized out>, signal_id=<optimized out>, detail=<optimized out>) at ../gobject/gsignal.c:3622
  #6  0x00007ffff6f75cda in _g_closure_invoke_va (param_types=0x0, n_params=<optimized out>, args=0x7fffffffd520, instance=0x8ee230, return_value=0x0, closure=0x8efbf0) at ../gobject/gclosure.c:895
  #7  g_signal_emit_valist (instance=0x8ee230, signal_id=216, detail=0, var_args=var_args@entry=0x7fffffffd520) at ../gobject/gsignal.c:3472
  #8  0x00007ffff6f75e53 in g_signal_emit (instance=instance@entry=0x8ee230, signal_id=<optimized out>, detail=detail@entry=0) at ../gobject/gsignal.c:3622
  #9  0x00007ffff76dd7dd in gtk_button_clicked (button=button@entry=0x8ee230) at ../gtk/gtkbutton.c:1541
  #10 0x00007ffff76dfad6 in gtk_button_finish_activate (do_it=1, button=0x8ee230) at ../gtk/gtkbutton.c:2042
  #11 button_activate_timeout (data=0x8ee230) at ../gtk/gtkbutton.c:1984
  #12 0x00007ffff7eddcad in gdk_threads_dispatch (data=data@entry=0xa5f470) at ../gdk/gdk.c:769
  #13 0x00007ffff6e55c69 in g_timeout_dispatch (source=0x711550, callback=0x7ffff7eddc80 <gdk_threads_dispatch>, user_data=0xa5f470) at ../glib/gmain.c:5054
  #14 0x00007ffff6e5539c in g_main_dispatch (context=0x4e7c70) at ../glib/gmain.c:3460
  #15 g_main_context_dispatch (context=0x4e7c70) at ../glib/gmain.c:4200
  #16 0x00007ffff6eb3438 in g_main_context_iterate.isra.0 (context=0x4e7c70, block=1, dispatch=1, self=<optimized out>) at ../glib/gmain.c:4276
  #17 0x00007ffff6e52a23 in g_main_context_iteration (context=context@entry=0x4e7c70, may_block=may_block@entry=1) at ../glib/gmain.c:4343
  #18 0x00007ffff708a01d in g_application_run (application=application@entry=0x4e5010, argc=<optimized out>, argv=0x7fffffffd9e8) at ../gio/gapplication.c:2573
  #19 0x00000000004133ad in main (argc=<optimized out>, argv=<optimized out>) at src/connection-editor/main.c:259

https://gitlab.gnome.org/GNOME/network-manager-applet/-/issues/178

Fixes: 3ff5b6cc9841

src/connection-editor/connection-helpers.c
@@ -272,8 +272,8 @@ import_vpn_from_file_cb (GtkWidget *dialog, gint response, gpointer user_data)
 	connection = connection_import_from_file (filename, G_TYPE_INVALID, NULL, &error);
 	if (!connection) {
 		/* pass */
-	} else if (nm_streq (nm_connection_get_connection_type (connection),
-	                     NM_SETTING_WIREGUARD_SETTING_NAME)) {
+	} else if (nm_streq0 (nm_connection_get_connection_type (connection),
+	                      NM_SETTING_WIREGUARD_SETTING_NAME)) {
 		info->result_func (FUNC_TAG_PAGE_NEW_CONNECTION_RESULT_CALL, connection, FALSE, NULL,
 		                   info->user_data);
 	} else {